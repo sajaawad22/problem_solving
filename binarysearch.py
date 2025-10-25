dfa86fdd15f14d7b8391556267ffba9cc287d137
Saja Awad <100625787+sajaawad22@users.noreply.github.com>
2025-10-25 09:36:59 +0300

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,15 @@
+def binarySearch(arr, target):
+    left, right =0, len(arr)-1
+    while left<=right:
+        mid =(right-left)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid]< target:
+            left= mid+1
+        else:
+            right =mid-1
+    return -1
+
+arr=[1,2,3,4,5,6,7,8,9]
+
+binarySearch(arr,3)
